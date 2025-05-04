de8a1987ec2bc9191fb5f17a1f3a47d3cbbb084b
Aitor <aitor.tsf@gmail.com>
2025-05-04 15:05:17 +0200

    add CorsConfig

--- Frontend/aitstore-frontend/src/environments/environment.prod.ts
+++ Frontend/aitstore-frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  production: true,
-  apiUrl: 'https://aitstore-backend.onrender.com/api'
+  production: false,
+  apiUrl: 'http://localhost:8081/api'
 };
--- Frontend/aitstore-frontend/src/environments/environment.ts
+++ Frontend/aitstore-frontend/src/environments/environment.ts
@@ -1,5 +1,4 @@
-// src/environments/environment.ts
 export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:8081/api'
+  production: true,
+  apiUrl: 'https://aitstore-backend.onrender.com/api'
 };
