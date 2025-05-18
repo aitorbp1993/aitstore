a10d0abf44eae44a7374163dc29e2c33b6e586bc
Aitor <aitor.tsf@gmail.com>
2025-05-18 16:56:25 +0200

    refactor pop-up carrito

--- Frontend/aitstore-frontend/src/app/layout.component.ts
+++ Frontend/aitstore-frontend/src/app/layout.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './shared/header.component';
 import { FooterComponent } from './shared/footer.component';
+import { FloatingCartButtonComponent } from "./shared/floating-cart-button.component";
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent, FloatingCartButtonComponent],
   styleUrls: ['./layout.component.scss'],
   templateUrl: './layout.component.html'
 })
