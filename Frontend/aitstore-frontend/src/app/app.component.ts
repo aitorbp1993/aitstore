e94f00bf42c106d99d8906b6396b6c4c8c50de2d
Aitor <aitor.tsf@gmail.com>
2025-05-28 19:13:48 +0200

    refactor carrito

--- Frontend/aitstore-frontend/src/app/app.component.ts
+++ Frontend/aitstore-frontend/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FloatingCartButtonComponent } from './shared/floating-cart-button.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FloatingCartButtonComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
